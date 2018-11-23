b8eead86ea3184530f9ef8132657032dae85a302
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2018-11-23 14:14:47 +1000

    Added new window, code needs to be fixed

---
+++ Interest search WPF/Interest search WPF/MessageAddWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Interest_Search_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MessageAddWindow.xaml
+    /// </summary>
+    public partial class MessageAddWindow : Window
+    {
+        public MessageAddWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
